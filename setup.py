e15c798b12888b8fc344992b1262143a9a6b276a
Matthew Hasselfield <mhasself@users.noreply.github.com>
2019-02-05 15:19:22 -0500

    versioneer.

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import versioneer
 
 setup(name='sotoddb',
-      version='1.0',
+      version=versioneer.get_version(),
       description='TOD Pipeline Database Library',
       author='Simons Observatory Collaboration',
       author_email='software@simonsobsveratory.org',
       url='https://simonsobservatory.org/software/',
       packages=['sotoddb'],
+      cmdclass=versioneer.get_cmdclass(),
      )
--- sotoddb/__init__.py
+++ sotoddb/__init__.py
@@ -6,3 +6,7 @@ from .detdb import ResultSet
 def get_example(db_type, *args, **kwargs):
     if db_type == 'DetDB':
         return detdb.get_example(*args, **kwargs)
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
