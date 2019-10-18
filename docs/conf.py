94cd67adcc7dd0e7e7861f75f32eba43ae200586
Matthew Hasselfield <mhasselfield@flatironinstitute.org>
2019-10-18 12:11:44 -0400

    Reorganize docs/*... require sphinx 2.0

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,7 @@
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
-# needs_sphinx = '1.0'
+needs_sphinx = '2.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
@@ -37,8 +37,11 @@ extensions = ['sphinx.ext.autodoc',
     'sphinx.ext.githubpages']
 
 # Present auto-documented members in source order (rather than alphabetical).
-autodoc_member_order = 'bysource'
-autoclass_content = 'class'
+autodoc_default_options = {
+    'member-order': 'bysource',
+}
+
+autoclass_content = 'both'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
